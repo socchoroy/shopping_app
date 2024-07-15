ad23d5237fa280efce0617faf3a6d03c1e5de4ad
soccho roy <socchoroy52@gmail.com>
2024-07-15 14:43:08 +0600

    Home Screen Initialize

---
+++ lib/widget/grocery_list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_item.dart';
+
+class GroceryList extends StatelessWidget {
+  const GroceryList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Groceries"),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+              groceryItems[index].name,
+            ),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              groceryItems[index].quantity.toString(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
